94f945d57e752ab00c46d5bdb6dc762af7309c41
Jean-Baptiste Emmanuel Zorg <zorgjeanb@gmail.com>
2021-12-22 20:11:05 +0100

    Add a login testsuite, with a simple good/bad password check

---
+++ tests/login.test.ts
@@ -0,0 +1,107 @@
+import { io, Socket } from "socket.io-client";
+import { DbClient } from "./db";
+
+describe("client", () => {
+
+	// Database connection
+	let DB: DbClient;
+	beforeAll(async () => {
+		DB = await DbClient.connectDatabase();
+	});
+
+	afterAll(async () => {
+		await DB.disconnect();
+	});
+
+	let client: Socket;
+
+	beforeEach((done) => {
+		client = io(`http://localhost:4288`);
+		client.on("connect", () => {
+			expect(client.connected).toBe(true);
+			done();
+		});
+	});
+
+	afterEach(() => {
+		if (client.connected) {
+			client.disconnect();
+		}
+		client.close();
+	});
+
+	// Test user cleanup
+	beforeEach(() => {
+		const accounts = DB.database.collection('Accounts');
+		accounts.deleteMany({ AccountName: "TESTACCOUNT" });
+	});
+	afterEach(() => {
+		const accounts = DB.database.collection('Accounts');
+		accounts.deleteMany({ AccountName: "TESTACCOUNT" });
+	});
+
+	describe('with a valid account', () => {
+
+		const accountData = {
+			Name: "Test",
+			AccountName: "TESTACCOUNT",
+			Password: "thisisabadpassword",
+			Email: "example@example.org"
+		};
+
+		beforeEach(async () => {
+			await DB.createAccount(accountData.AccountName, accountData.Password, accountData.Name, accountData.Email);
+		});
+
+		test("can login", (done) => {
+			expect.assertions(2);
+
+			client.on("LoginResponse", (arg) => {
+				expect(arg).toMatchObject({
+					AccountName: accountData.AccountName,
+					Name: accountData.Name,
+					Money: expect.any(Number),
+					Creation: expect.any(Number),
+					LastLogin: expect.any(Number),
+					Lovership: expect.any(Array),
+					MemberNumber: expect.any(Number),
+					ID: expect.any(String),
+					Environment: expect.any(String),
+					ItemPermission: expect.any(Number),
+					WhiteList: expect.any(Array),
+					BlackList: expect.any(Array),
+					FriendList: expect.any(Array),
+				});
+				done();
+			});
+
+			client.emit("AccountLogin", { AccountName: accountData.AccountName, Password: accountData.Password });
+		});
+	});
+
+	describe('with an valid account', () => {
+
+		const accountData = {
+			Name: "Test",
+			AccountName: "TESTACCOUNT",
+			Password: "thisisabadpassword",
+			Email: "example@example.org"
+		};
+
+		beforeEach(async () => {
+			await DB.createAccount(accountData.AccountName, accountData.Password, accountData.Name, accountData.Email);
+		});
+
+		test("can't login with an incorrect password", (done) => {
+			expect.assertions(2);
+
+			client.on("LoginResponse", (arg) => {
+				expect(arg).toBe("InvalidNamePassword");
+				done();
+			});
+
+			client.emit("AccountLogin", { AccountName: accountData.AccountName, Password: "not the correct password" });
+		});
+	});
+
+});
